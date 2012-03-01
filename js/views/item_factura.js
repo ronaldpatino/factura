fa8f959a7cd7776f2f0b86407e00dcd10daf83ff
unknown <ba01000660@ba01000660.austro.grpfin>
2012-03-01 09:59:09 -0500

    Calculating totals and taxes when the numer of item changes from the dropdown list

--- js/views/item_factura.js
+++ js/views/item_factura.js
@@ -23,17 +23,27 @@ window.ItemFacturaView = Backbone.View.extend({
     },
 
     calcularValores: function() {
+        this.getVariablesInterfaz();
         this.calcularSubTotal();
         this.calcularDescuento();
+        this.calcularIva();
+    },
 
+    getVariablesInterfaz: function(){
+        this.descuento_interfaz = window.$('#descuento').val();
+        this.descuento_tipo_interfaz = window.$('#descuento_tipo').val();
+        this.iva_tipo_interfaz = window.$('#iva_tipo').val();
+        this.cantidad_devuelta_interfaz = this.$('#cantidad_select').val();
     },
 
     calcularSubTotal: function(){
-        this.model.set('cantidad_devuelta',this.$('#cantidad_select').val());
+
+
+        this.model.set('cantidad_devuelta',this.cantidad_devuelta_interfaz);
 
         var valor_total =   this.model.get('cantidad_devuelta') *
-            this.model.get('dias_renovar') *
-            this.model.get('valor_unitario');
+                            this.model.get('dias_renovar') *
+                            this.model.get('valor_unitario');
 
         this.model.set('valor_total', valor_total);
 
@@ -44,17 +54,59 @@ window.ItemFacturaView = Backbone.View.extend({
         }, this);
 
         this.model.get('factura_id').set('subtotal',subtotal);
+        this.model.get('factura_id').set('descuento',this.descuento_interfaz);
     },
 
     calcularDescuento: function(){
 
-        var descuento_interfaz = window.$('#descuento').val();
-        alert(descuento_interfaz);
-        var descuento_ui = this.model.get('factura_id').get('subtotal') - descuento_interfaz;
+        if (this.descuento_tipo_interfaz == 1)
+        {
+            if (this.model.get('factura_id').get('subtotal') > this.descuento_interfaz)
+            {
+                var descuento_interfaz = this.descuento_interfaz;
+                var descuento_ui = this.model.get('factura_id').get('subtotal') - this.descuento_interfaz;
+                this.model.get('factura_id').set('descuento_ui', descuento_ui);
+                this.model.get('factura_id').set('descuento_tipo', this.descuento_tipo_interfaz);
+            }
+            else
+            {
+                this.model.get('factura_id').set('descuento_ui', this.model.get('factura_id').get('subtotal'));
+                this.model.get('factura_id').set('descuento', 0);
+            }
+        }
+        else
+        {
+            if (this.descuento_interfaz < 100)
+            {
+                var descuento_porcentaje = (this.model.get('factura_id').get('subtotal') * this.descuento_interfaz)/100;
+                var descuento_ui = this.model.get('factura_id').get('subtotal') - descuento_porcentaje;
+                this.model.get('factura_id').set('descuento_ui', descuento_ui);
+                this.model.get('factura_id').set('descuento_tipo', this.descuento_tipo_interfaz);
+            }
+            else
+            {
+                this.model.get('factura_id').set('descuento_ui', this.model.get('factura_id').get('subtotal'));
+                this.model.get('factura_id').set('descuento', 0);
+            }
+        }
+    },
 
-        this.model.set('descuento_ui', descuento_ui);
+    calcularIva: function () {
+        if (this.model.get('factura_id').get('subtotal') > 0 ) {
+            if (this.iva_tipo_interfaz == 12)
+            {
+                var valor_iva = this.model.get('factura_id').get('descuento_ui') * 0.12;
+                this.model.get('factura_id').set('iva_tipo', 12);
+            }
+            else
+            {
+                var valor_iva = 0;
+                this.model.get('factura_id').set('iva_tipo', 0);
+            }
 
+            this.model.get('factura_id').set('iva', valor_iva);
 
+        }
     }
 
 })
